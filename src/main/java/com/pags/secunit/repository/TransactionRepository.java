aeab46c295a601ee36ea1317f9db0ebf89d76cf0
Guilherme Fujii Filgueiras <gfilgueiras@uolinc.com>
2022-10-18 13:46:21 -0300

    fix: added @param to method

--- src/main/java/com/pags/secunit/repository/TransactionRepository.java
+++ src/main/java/com/pags/secunit/repository/TransactionRepository.java
@@ -3,13 +3,13 @@ package com.pags.secunit.repository;
 import com.pags.secunit.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
-
     @Query(value = "SELECT * FROM TRANSACTION WHERE SENT_ID = :id", nativeQuery = true)
-    List<Transaction> allTransactionsOf(Integer id);
+    List<Transaction> allTransactionsOf(@Param("id") Integer id);
 }
