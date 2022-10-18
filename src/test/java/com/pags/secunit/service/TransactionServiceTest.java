1a7d98519f9e9f5cbfa4416085938e72bdbb6b19
Guilherme Fujii Filgueiras <gfilgueiras@uolinc.com>
2022-10-18 14:08:48 -0300

    chore: exs

--- src/test/java/com/pags/secunit/service/TransactionServiceTest.java
+++ src/test/java/com/pags/secunit/service/TransactionServiceTest.java
@@ -10,5 +10,17 @@ public class TransactionServiceTest {
     @InjectMocks
     private TransactionService transactionService;
 
+    // 1 - Trate um lancamento de excecoes
+
+    // 2 - Valide uma entrada de senha do usuario para se registrar com a excecao
+
+    // 3 - Valide o input no valor da transacao e a excecao
+
+    // 4 - Valide o tamanho do valor da transacao e a excecao
+
+    // 5 - Valide o retorno das respostas dos servicos de transacao
+    // Dica: Todas as informacoes de retorno sao necessarias?
+
+    // 6 - Intentifique e valide um ataque de Broken Object Level Access
 
 }
