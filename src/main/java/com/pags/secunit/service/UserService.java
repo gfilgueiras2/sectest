2af1cfcf36ab5abd58eec6df73fe41d0123b0a77
Guilherme Fujii Filgueiras <gfilgueiras@uolinc.com>
2022-10-19 13:47:02 -0300

    fix: added exception and logic to info service

--- src/main/java/com/pags/secunit/service/UserService.java
+++ src/main/java/com/pags/secunit/service/UserService.java
@@ -2,8 +2,11 @@ package com.pags.secunit.service;
 
 import com.pags.secunit.entity.User;
 import com.pags.secunit.repository.UserRepository;
+import dto.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 @RequiredArgsConstructor
@@ -11,8 +14,21 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public User getUserInfo(Integer id) {
-        return userRepository.getReferenceById(id);
+    public BaseResponse<User> getUserInfo(Integer id) throws Exception {
+        try {
+            User user = userRepository.getReferenceById(id);
+            String maskAddress = user.getAddress().substring(0,6);
+            user.setAddress(maskAddress);
+            return BaseResponse.<User>builder()
+                    .response(user)
+                    .error(null)
+                    .build();
+        } catch (Exception e) {
+            return BaseResponse.<User>builder()
+                    .response(null)
+                    .error(e.getMessage())
+                    .build();
+        }
     }
 
     public User createAccount(User user) {
