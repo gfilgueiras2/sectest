eef3bd10ae98cecfc85d2406cd0d4b275360f840
Guilherme Fujii Filgueiras <gfilgueiras@uolinc.com>
2022-10-21 09:32:41 -0300

    fix: adjust on the exs description

--- src/test/java/com/pags/secunit/service/UserServiceTest.java
+++ src/test/java/com/pags/secunit/service/UserServiceTest.java
@@ -14,12 +14,4 @@ public class UserServiceTest {
 
     // 2 - Valide uma entrada de senha do usuario para se registrar com a excecao
 
-    // 3 - Valide o input no valor da transacao e a excecao
-
-    // 4 - Valide o tamanho do valor da transacao e a excecao
-
-    // 5 - Valide o retorno das respostas dos servicos de transacao
-    // Dica: Todas as informacoes de retorno sao necessarias?
-
-    // 6 - Intentifique e valide um ataque de Broken Object Level Access
 }
